46dd85658bbdb5f92baa7d500794e31947981454
Chris_Eteka_Dellcls <christopher.eteka.200986@unn.edu.ng>
2020-08-03 09:43:34 +0100

    Hot fix

--- src/main/java/com/chrisworks/personal/inventorysystem/Backend/Entities/POJO/Plan.java
+++ src/main/java/com/chrisworks/personal/inventorysystem/Backend/Entities/POJO/Plan.java
@@ -1,9 +1,7 @@
 package com.chrisworks.personal.inventorysystem.Backend.Entities.POJO;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -15,7 +13,8 @@ import java.util.Set;
  * @since 8/1/2020
  * @email chriseteka@gmail.com
  */
-@Data
+@Getter
+@Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -54,4 +53,50 @@ public class Plan {
     @OneToMany(mappedBy = "plan", targetEntity = BusinessOwner.class, cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     private Set<BusinessOwner> businesses = new HashSet<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Plan)) return false;
+
+        Plan plan = (Plan) o;
+
+        if (getNumberOfStaff() != plan.getNumberOfStaff()) return false;
+        if (getNumberOfWarehouses() != plan.getNumberOfWarehouses()) return false;
+        if (getNumberOfShops() != plan.getNumberOfShops()) return false;
+        if (getPlanId() != null ? !getPlanId().equals(plan.getPlanId()) : plan.getPlanId() != null) return false;
+        if (getCreatedDate() != null ? !getCreatedDate().equals(plan.getCreatedDate()) : plan.getCreatedDate() != null)
+            return false;
+        if (getCreatedTime() != null ? !getCreatedTime().equals(plan.getCreatedTime()) : plan.getCreatedTime() != null)
+            return false;
+        if (getUpdateDate() != null ? !getUpdateDate().equals(plan.getUpdateDate()) : plan.getUpdateDate() != null)
+            return false;
+        return getPlanName() != null ? getPlanName().equals(plan.getPlanName()) : plan.getPlanName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getPlanId() != null ? getPlanId().hashCode() : 0;
+        result = 31 * result + (getCreatedDate() != null ? getCreatedDate().hashCode() : 0);
+        result = 31 * result + (getCreatedTime() != null ? getCreatedTime().hashCode() : 0);
+        result = 31 * result + (getUpdateDate() != null ? getUpdateDate().hashCode() : 0);
+        result = 31 * result + (getPlanName() != null ? getPlanName().hashCode() : 0);
+        result = 31 * result + getNumberOfStaff();
+        result = 31 * result + getNumberOfWarehouses();
+        result = 31 * result + getNumberOfShops();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" +
+                "planId=" + planId +
+                ", createdDate=" + createdDate +
+                ", createdTime=" + createdTime +
+                ", updateDate=" + updateDate +
+                ", planName='" + planName + '\'' +
+                ", numberOfStaff=" + numberOfStaff +
+                ", numberOfWarehouses=" + numberOfWarehouses +
+                ", numberOfShops=" + numberOfShops +
+                '}';
+    }
 }
