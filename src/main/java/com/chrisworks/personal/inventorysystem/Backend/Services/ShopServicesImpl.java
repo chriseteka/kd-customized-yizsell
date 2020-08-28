36131e8e7232c8aae8c304b4562e552919fa6cf1
Chris_Eteka_Dellcls <christopher.eteka.200986@unn.edu.ng>
2020-08-28 01:43:44 +0100

    Completed receiving of stock from local

--- src/main/java/com/chrisworks/personal/inventorysystem/Backend/Services/ShopServicesImpl.java
+++ src/main/java/com/chrisworks/personal/inventorysystem/Backend/Services/ShopServicesImpl.java
@@ -8,11 +8,17 @@ import com.chrisworks.personal.inventorysystem.Backend.ExceptionManagement.Inven
 import com.chrisworks.personal.inventorysystem.Backend.ExceptionManagement.InventoryAPIExceptions.InventoryAPIResourceNotFoundException;
 import com.chrisworks.personal.inventorysystem.Backend.Repositories.*;
 import com.chrisworks.personal.inventorysystem.Backend.Utility.AuthenticatedUserDetails;
+import com.chrisworks.personal.inventorysystem.Backend.Websocket.models.UserMiniProfile;
+import com.chrisworks.personal.inventorysystem.Backend.Websocket.repoServices.UserMiniProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static com.chrisworks.personal.inventorysystem.Backend.Utility.Utility.toSingleton;
 
 
 /**
@@ -24,14 +30,46 @@ import java.util.stream.Collectors;
 public class ShopServicesImpl implements ShopServices {
 
     private final ShopRepository shopRepository;
-
+    private final CustomerRepository customerRepository;
+    private final ExpenseRepository expenseRepository;
+    private final IncomeRepository incomeRepository;
+    private final ReturnedStockRepository returnedStockRepository;
+    private final InvoiceRepository invoiceRepository;
+    private final ShopStocksRepository shopStocksRepository;
+    private final StockCategoryRepository stockCategoryRepository;
+    private final SupplierRepository supplierRepository;
+    private final SellerRepository sellerRepository;
+    private final UserMiniProfileRepository userMiniProfileRepository;
+    private final StockSoldRepository stockSoldRepository;
+    private final MultiplePaymentModeRepository multiplePaymentModeRepository;
     private final BusinessOwnerRepository businessOwnerRepository;
+    private Map<String, StockCategory> stockCategoryMap;
+    private Map<String, Supplier> supplierMap;
+    private Map<String, Customer> customerMap;
+
 
     @Autowired
-    public ShopServicesImpl(ShopRepository shopRepository,
-                            BusinessOwnerRepository businessOwnerRepository) {
+    public ShopServicesImpl(ShopRepository shopRepository, BusinessOwnerRepository businessOwnerRepository,
+                            CustomerRepository customerRepository, ExpenseRepository expenseRepository,
+                            IncomeRepository incomeRepository, ReturnedStockRepository returnedStockRepository,
+                            InvoiceRepository invoiceRepository, ShopStocksRepository shopStocksRepository,
+                            StockCategoryRepository stockCategoryRepository, SupplierRepository supplierRepository,
+                            SellerRepository sellerRepository, UserMiniProfileRepository userMiniProfileRepository,
+                            StockSoldRepository stockSoldRepository, MultiplePaymentModeRepository multiplePaymentModeRepository) {
         this.shopRepository = shopRepository;
         this.businessOwnerRepository = businessOwnerRepository;
+        this.customerRepository = customerRepository;
+        this.expenseRepository = expenseRepository;
+        this.incomeRepository = incomeRepository;
+        this.returnedStockRepository = returnedStockRepository;
+        this.invoiceRepository = invoiceRepository;
+        this.shopStocksRepository = shopStocksRepository;
+        this.stockCategoryRepository = stockCategoryRepository;
+        this.supplierRepository = supplierRepository;
+        this.sellerRepository = sellerRepository;
+        this.userMiniProfileRepository = userMiniProfileRepository;
+        this.stockSoldRepository = stockSoldRepository;
+        this.multiplePaymentModeRepository = multiplePaymentModeRepository;
     }
 
     @Override
@@ -140,10 +178,96 @@ public class ShopServicesImpl implements ShopServices {
     }
 
     @Override
+    @Transactional
     public ResponseObject receiveDesktopPush(DesktopPushObject desktopPushObject) {
 
-        System.out.println("Received**** " + desktopPushObject.toString());
-        return new ResponseObject(true, "Push completed.");
+        List<Customer> customers = Collections.emptyList();
+        List<Expense> expenses = Collections.emptyList();
+        List<Income> incomes = Collections.emptyList();
+        List<ReturnedStock> returnedStocks = Collections.emptyList();
+        List<Invoice> invoices = Collections.emptyList();
+        List<ShopStocks> shopStocks;
+        boolean status;
+
+        List<Seller> sellerList = sellerRepository.findAllByCreatedBy(desktopPushObject.getShop().getCreatedBy());
+        Seller authSeller = sellerList.stream().filter(seller -> seller.getSellerEmail()
+                .equalsIgnoreCase(AuthenticatedUserDetails.getUserFullName())).collect(toSingleton());
+        UserMiniProfile authUserMiniProfile = userMiniProfileRepository.findDistinctByEmail(authSeller.getSellerEmail());
+        List<String> sellers = sellerList.stream().map(Seller::getSellerEmail).collect(Collectors.toList());
+        sellers.add(desktopPushObject.getShop().getCreatedBy());
+        readyCustomerMap(sellers);
+
+        //Settled
+        if (!desktopPushObject.getCustomers().isEmpty()) {
+            List<Customer> customerList = new ArrayList<>();
+            desktopPushObject.getCustomers().forEach(customer -> {
+                customer.setCustomerId(null);
+                if (customerMap.containsKey(customer.getCustomerPhoneNumber()))
+                    customer.setCustomerId(customerMap.get(customer.getCustomerPhoneNumber()).getCustomerId());
+                customerList.add(customer);
+            });
+           customers = customerRepository.saveAll(customerList);
+        }
+
+        customerMap.clear();
+        readyCustomerMap(sellers);
+
+        //Settled
+        if (!desktopPushObject.getExpenses().isEmpty())
+            expenses = expenseRepository.saveAll(desktopPushObject.getExpenses().stream()
+                .peek(e -> {
+                    e.setExpenseId(null);
+                    e.setShop(desktopPushObject.getShop());
+                    e.setExpenseTypeVal(String.valueOf(e.getExpenseTypeValue()));
+                }).collect(Collectors.toList()));
+
+        //Settled
+        if (!desktopPushObject.getIncomes().isEmpty())
+            incomes = incomeRepository.saveAll(desktopPushObject.getIncomes().stream()
+                .peek(e -> {
+                    e.setIncomeId(null);
+                    e.setShop(desktopPushObject.getShop());
+                    e.setIncomeTypeVal(String.valueOf(e.getIncomeTypeValue()));
+                }).collect(Collectors.toList()));
+
+        //Settled
+        if (!desktopPushObject.getReturnedStocks().isEmpty())
+            returnedStocks = returnedStockRepository.saveAll(desktopPushObject.getReturnedStocks().stream()
+                .peek(e -> {
+                    e.setReturnedStockId(null);
+                    e.setShop(desktopPushObject.getShop());
+                    if (e.getCustomerId() != null)
+                        e.setCustomerId(customerMap.get(e.getCustomerId().getCustomerPhoneNumber()));
+                }).collect(Collectors.toList()));
+
+        //Settled
+        if (!desktopPushObject.getInvoices().isEmpty())
+            invoices = invoiceRepository.saveAll(desktopPushObject.getInvoices().stream()
+                .peek(e -> {
+                    e.setInvoiceId(null);
+                    e.setSeller(authSeller);
+                    e.setSoldBy(authUserMiniProfile);
+                    e.setStockSold(persistStockSold(e));
+                    e.setShop(desktopPushObject.getShop());
+                    if (!e.getMultiplePayment().isEmpty())
+                        e.setMultiplePayment(persistMultiplePay(e));
+                    if (e.getCustomerId() != null)
+                        e.setCustomerId(customerMap.get(e.getCustomerId().getCustomerPhoneNumber()));
+                    e.setPaymentModeVal(String.valueOf(e.getPaymentModeValue()));
+                }).collect(Collectors.toList()));
+
+        //Settled
+        shopStocks = syncShopStock(desktopPushObject.getShop(), desktopPushObject.getShopStocks(), sellers);
+
+        status = (customers.size() == desktopPushObject.getCustomers().size())
+                && (expenses.size() == desktopPushObject.getExpenses().size())
+                && (incomes.size() == desktopPushObject.getIncomes().size())
+                && (returnedStocks.size() == desktopPushObject.getReturnedStocks().size())
+                && (invoices.size() == desktopPushObject.getInvoices().size())
+                && (shopStocks.size() == desktopPushObject.getShopStocks().size());
+
+        return new ResponseObject(status, status ? "Push Completed Successfully"
+                : "Push could not complete, please try again.");
     }
 
     private void verifyShopCreationLimitViolation(BusinessOwner businessOwner) {
@@ -153,4 +277,105 @@ public class ShopServicesImpl implements ShopServices {
             throw new InventoryAPIOperationException("Operation not allowed",
                     "You have reached the maximum number of shops you can create in your plan/subscription", null);
     }
+
+    private List<ShopStocks> syncShopStock(Shop shopInContext, List<ShopStocks> incomingStock, List<String> sellers){
+
+        List<ShopStocks> shopStocksToSave = new ArrayList<>();
+
+        List<ShopStocks> existingStock = shopStocksRepository.findAllByShop(shopInContext);
+        List<String> existingNames = existingStock.stream().map(ShopStocks::getStockName).collect(Collectors.toList());
+
+        if (existingStock.isEmpty()) return Collections.emptyList();
+
+        incomingStock.forEach(sc -> {
+
+            String incomingStockName = sc.getStockName();
+            if (!existingNames.contains(incomingStockName)) return;
+            if (existingNames.contains(incomingStockName)){
+
+                sc.setShopStockId(existingStock.stream()
+                        .filter(es -> es.getStockName().equalsIgnoreCase(incomingStockName))
+                        .limit(1).collect(toSingleton()).getShopStockId());
+                shopStocksToSave.add(setShopStockTransients(sc, shopInContext, sellers));
+            }
+        });
+
+        if (!shopStocksToSave.isEmpty()) return shopStocksRepository.saveAll(shopStocksToSave);
+
+        return Collections.emptyList();
+    }
+
+    private ShopStocks setShopStockTransients(ShopStocks sc, Shop shopInContext, List<String> sellers){
+
+        readyStockCategoryMap(sellers);
+        readySupplierMap(sellers);
+        String categoryName = sc.getStockCategory().getCategoryName();
+
+        if (stockCategoryMap.isEmpty() || !stockCategoryMap.containsKey(categoryName))
+            stockCategoryMap.put(categoryName, stockCategoryRepository.save(sc.getStockCategory()));
+        sc.setStockCategory(stockCategoryMap.get(categoryName));
+
+        if (null != sc.getLastRestockPurchasedFrom()
+                && StringUtils.hasLength(sc.getLastRestockPurchasedFrom().getSupplierPhoneNumber())) {
+
+            String supplierPhoneNumber = sc.getLastRestockPurchasedFrom().getSupplierPhoneNumber();
+            if (supplierMap.isEmpty() || !supplierMap.containsKey(supplierPhoneNumber))
+                supplierMap.put(supplierPhoneNumber, supplierRepository.save(sc.getLastRestockPurchasedFrom()));
+            sc.setLastRestockPurchasedFrom(supplierMap.get(supplierPhoneNumber));
+        }
+
+        if (!sc.getStockPurchasedFrom().isEmpty()) {
+            Set<Supplier> supplierSet = new HashSet<>();
+            for (Supplier supplier : sc.getStockPurchasedFrom()) {
+                String supplierPhoneNumber = supplier.getSupplierPhoneNumber();
+                if (StringUtils.hasText(supplierPhoneNumber)) {
+                    if (supplierMap.isEmpty() || !supplierMap.containsKey(supplierPhoneNumber))
+                        supplierMap.put(supplierPhoneNumber, supplierRepository.save(sc.getLastRestockPurchasedFrom()));
+                    supplierSet.add(supplierMap.get(supplierPhoneNumber));
+                }
+            }
+            if (!supplierSet.isEmpty()) sc.setStockPurchasedFrom(supplierSet);
+        }
+
+        sc.setShop(shopInContext);
+
+        return sc;
+    }
+
+    private void readyStockCategoryMap(List<String> sellers){
+        stockCategoryMap = new HashMap<>();
+        stockCategoryMap.putAll(sellers.stream().map(
+            stockCategoryRepository::findAllByCreatedBy).flatMap(List::parallelStream)
+                .collect(Collectors.toMap(StockCategory::getCategoryName, stockCategory -> stockCategory)));
+    }
+
+    private void readySupplierMap(List<String> sellers){
+        supplierMap = new HashMap<>();
+        supplierMap.putAll(sellers.stream().map(
+            supplierRepository::findAllByCreatedBy).flatMap(List::parallelStream)
+                .collect(Collectors.toMap(Supplier::getSupplierPhoneNumber, supplier -> supplier)));
+    }
+
+    private void readyCustomerMap(List<String> sellers){
+        customerMap = new HashMap<>();
+        customerMap.putAll(sellers.stream().map(
+            customerRepository::findAllByCreatedBy).flatMap(List::parallelStream)
+                .collect(Collectors.toMap(Customer::getCustomerPhoneNumber, customer -> customer)));
+    }
+
+    private Set<StockSold> persistStockSold(Invoice invoice){
+
+        return new HashSet<>(stockSoldRepository.saveAll(invoice.getStockSold().stream()
+                .peek(ss -> ss.setStockSoldId(null)).collect(Collectors.toList())));
+    }
+
+    private List<MultiplePaymentMode> persistMultiplePay(Invoice invoice){
+
+        return multiplePaymentModeRepository.saveAll(
+                invoice.getMultiplePayment().stream().peek(ss -> {
+                    ss.setMultiplePaymentModeId(null);
+                    ss.setPaymentModeVal(String.valueOf(ss.getPaymentModeValue()));
+                }).collect(Collectors.toList()));
+    }
+
 }
