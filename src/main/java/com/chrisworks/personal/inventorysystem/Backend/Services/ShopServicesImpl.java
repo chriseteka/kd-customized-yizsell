3894ace9a3b131a56b17f29bd3bbeba3f117ef49
Chris_Eteka_Dellcls <christopher.eteka.200986@unn.edu.ng>
2020-08-29 13:30:23 +0100

    Clead up online receive sync

--- src/main/java/com/chrisworks/personal/inventorysystem/Backend/Services/ShopServicesImpl.java
+++ src/main/java/com/chrisworks/personal/inventorysystem/Backend/Services/ShopServicesImpl.java
@@ -13,7 +13,6 @@ import com.chrisworks.personal.inventorysystem.Backend.Websocket.repoServices.Us
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -36,15 +35,11 @@ public class ShopServicesImpl implements ShopServices {
     private final ReturnedStockRepository returnedStockRepository;
     private final InvoiceRepository invoiceRepository;
     private final ShopStocksRepository shopStocksRepository;
-    private final StockCategoryRepository stockCategoryRepository;
-    private final SupplierRepository supplierRepository;
     private final SellerRepository sellerRepository;
     private final UserMiniProfileRepository userMiniProfileRepository;
     private final StockSoldRepository stockSoldRepository;
     private final MultiplePaymentModeRepository multiplePaymentModeRepository;
     private final BusinessOwnerRepository businessOwnerRepository;
-    private Map<String, StockCategory> stockCategoryMap;
-    private Map<String, Supplier> supplierMap;
     private Map<String, Customer> customerMap;
 
 
@@ -53,7 +48,6 @@ public class ShopServicesImpl implements ShopServices {
                             CustomerRepository customerRepository, ExpenseRepository expenseRepository,
                             IncomeRepository incomeRepository, ReturnedStockRepository returnedStockRepository,
                             InvoiceRepository invoiceRepository, ShopStocksRepository shopStocksRepository,
-                            StockCategoryRepository stockCategoryRepository, SupplierRepository supplierRepository,
                             SellerRepository sellerRepository, UserMiniProfileRepository userMiniProfileRepository,
                             StockSoldRepository stockSoldRepository, MultiplePaymentModeRepository multiplePaymentModeRepository) {
         this.shopRepository = shopRepository;
@@ -64,8 +58,6 @@ public class ShopServicesImpl implements ShopServices {
         this.returnedStockRepository = returnedStockRepository;
         this.invoiceRepository = invoiceRepository;
         this.shopStocksRepository = shopStocksRepository;
-        this.stockCategoryRepository = stockCategoryRepository;
-        this.supplierRepository = supplierRepository;
         this.sellerRepository = sellerRepository;
         this.userMiniProfileRepository = userMiniProfileRepository;
         this.stockSoldRepository = stockSoldRepository;
@@ -186,7 +178,7 @@ public class ShopServicesImpl implements ShopServices {
         List<Income> incomes = Collections.emptyList();
         List<ReturnedStock> returnedStocks = Collections.emptyList();
         List<Invoice> invoices = Collections.emptyList();
-        List<ShopStocks> shopStocks;
+        List<ShopStocks> shopStocks = Collections.emptyList();
         boolean status;
 
         Shop shop = shopRepository.findById(desktopPushObject.getShopId())
@@ -201,7 +193,7 @@ public class ShopServicesImpl implements ShopServices {
         readyCustomerMap(sellers);
 
         //Settled
-        if (!desktopPushObject.getCustomers().isEmpty()) {
+        if (desktopPushObject.getCustomers() != null && !desktopPushObject.getCustomers().isEmpty()) {
             List<Customer> customerList = new ArrayList<>();
             desktopPushObject.getCustomers().forEach(customer -> {
                 customer.setCustomerId(null);
@@ -216,7 +208,7 @@ public class ShopServicesImpl implements ShopServices {
         readyCustomerMap(sellers);
 
         //Settled
-        if (!desktopPushObject.getExpenses().isEmpty())
+        if (desktopPushObject.getExpenses() != null && !desktopPushObject.getExpenses().isEmpty())
             expenses = expenseRepository.saveAll(desktopPushObject.getExpenses().stream()
                 .peek(e -> {
                     e.setExpenseId(null);
@@ -225,7 +217,7 @@ public class ShopServicesImpl implements ShopServices {
                 }).collect(Collectors.toList()));
 
         //Settled
-        if (!desktopPushObject.getIncomes().isEmpty())
+        if (desktopPushObject.getIncomes() != null && !desktopPushObject.getIncomes().isEmpty())
             incomes = incomeRepository.saveAll(desktopPushObject.getIncomes().stream()
                 .peek(e -> {
                     e.setIncomeId(null);
@@ -234,7 +226,7 @@ public class ShopServicesImpl implements ShopServices {
                 }).collect(Collectors.toList()));
 
         //Settled
-        if (!desktopPushObject.getReturnedStocks().isEmpty())
+        if (desktopPushObject.getReturnedStocks() != null && !desktopPushObject.getReturnedStocks().isEmpty())
             returnedStocks = returnedStockRepository.saveAll(desktopPushObject.getReturnedStocks().stream()
                 .peek(e -> {
                     e.setReturnedStockId(null);
@@ -244,7 +236,7 @@ public class ShopServicesImpl implements ShopServices {
                 }).collect(Collectors.toList()));
 
         //Settled
-        if (!desktopPushObject.getInvoices().isEmpty())
+        if (desktopPushObject.getInvoices() != null && !desktopPushObject.getInvoices().isEmpty())
             invoices = invoiceRepository.saveAll(desktopPushObject.getInvoices().stream()
                 .peek(e -> {
                     e.setInvoiceId(null);
@@ -260,14 +252,15 @@ public class ShopServicesImpl implements ShopServices {
                 }).collect(Collectors.toList()));
 
         //Settled
-        shopStocks = syncShopStock(shop, desktopPushObject.getShopStocks(), sellers);
+        if (desktopPushObject.getShopStocks() != null && !desktopPushObject.getShopStocks().isEmpty())
+            shopStocks = syncShopStock(shop, desktopPushObject.getShopStocks());
 
-        status = (customers.size() == desktopPushObject.getCustomers().size())
-                && (expenses.size() == desktopPushObject.getExpenses().size())
-                && (incomes.size() == desktopPushObject.getIncomes().size())
-                && (returnedStocks.size() == desktopPushObject.getReturnedStocks().size())
-                && (invoices.size() == desktopPushObject.getInvoices().size())
-                && (shopStocks.size() == desktopPushObject.getShopStocks().size());
+        status = (desktopPushObject.getCustomers() == null || customers.size() == desktopPushObject.getCustomers().size())
+                && (desktopPushObject.getExpenses() == null || expenses.size() == desktopPushObject.getExpenses().size())
+                && (desktopPushObject.getIncomes() == null || incomes.size() == desktopPushObject.getIncomes().size())
+                && (desktopPushObject.getReturnedStocks() == null || returnedStocks.size() == desktopPushObject.getReturnedStocks().size())
+                && (desktopPushObject.getInvoices() == null || invoices.size() == desktopPushObject.getInvoices().size())
+                && (desktopPushObject.getShopStocks() == null || shopStocks.size() == desktopPushObject.getShopStocks().size());
 
         return new ResponseObject(status, status ? "Push Completed Successfully"
                 : "Push could not complete, please try again.");
@@ -281,7 +274,7 @@ public class ShopServicesImpl implements ShopServices {
                     "You have reached the maximum number of shops you can create in your plan/subscription", null);
     }
 
-    private List<ShopStocks> syncShopStock(Shop shopInContext, List<ShopStocks> incomingStock, List<String> sellers){
+    private List<ShopStocks> syncShopStock(Shop shopInContext, List<ShopStocks> incomingStock){
 
         List<ShopStocks> shopStocksToSave = new ArrayList<>();
 
@@ -296,67 +289,22 @@ public class ShopServicesImpl implements ShopServices {
             if (!existingNames.contains(incomingStockName)) return;
             if (existingNames.contains(incomingStockName)){
 
-                sc.setShopStockId(existingStock.stream()
+                ShopStocks existingShopStock = existingStock.stream()
                         .filter(es -> es.getStockName().equalsIgnoreCase(incomingStockName))
-                        .limit(1).collect(toSingleton()).getShopStockId());
-                shopStocksToSave.add(setShopStockTransients(sc, shopInContext, sellers));
+                        .limit(1).collect(toSingleton());
+
+                sc.setShop(shopInContext);
+                sc.setShopStockId(existingShopStock.getShopStockId());
+                sc.setStockCategory(existingShopStock.getStockCategory());
+                sc.setStockPurchasedFrom(existingShopStock.getStockPurchasedFrom());
+                sc.setLastRestockPurchasedFrom(existingShopStock.getLastRestockPurchasedFrom());
+                shopStocksToSave.add(sc);
             }
         });
 
         if (!shopStocksToSave.isEmpty()) return shopStocksRepository.saveAll(shopStocksToSave);
 
         return Collections.emptyList();
-    }
-
-    private ShopStocks setShopStockTransients(ShopStocks sc, Shop shopInContext, List<String> sellers){
-
-        readyStockCategoryMap(sellers);
-        readySupplierMap(sellers);
-        String categoryName = sc.getStockCategory().getCategoryName();
-
-        if (stockCategoryMap.isEmpty() || !stockCategoryMap.containsKey(categoryName))
-            stockCategoryMap.put(categoryName, stockCategoryRepository.save(sc.getStockCategory()));
-        sc.setStockCategory(stockCategoryMap.get(categoryName));
-
-        if (null != sc.getLastRestockPurchasedFrom()
-                && StringUtils.hasLength(sc.getLastRestockPurchasedFrom().getSupplierPhoneNumber())) {
-
-            String supplierPhoneNumber = sc.getLastRestockPurchasedFrom().getSupplierPhoneNumber();
-            if (supplierMap.isEmpty() || !supplierMap.containsKey(supplierPhoneNumber))
-                supplierMap.put(supplierPhoneNumber, supplierRepository.save(sc.getLastRestockPurchasedFrom()));
-            sc.setLastRestockPurchasedFrom(supplierMap.get(supplierPhoneNumber));
-        }
-
-        if (!sc.getStockPurchasedFrom().isEmpty()) {
-            Set<Supplier> supplierSet = new HashSet<>();
-            for (Supplier supplier : sc.getStockPurchasedFrom()) {
-                String supplierPhoneNumber = supplier.getSupplierPhoneNumber();
-                if (StringUtils.hasText(supplierPhoneNumber)) {
-                    if (supplierMap.isEmpty() || !supplierMap.containsKey(supplierPhoneNumber))
-                        supplierMap.put(supplierPhoneNumber, supplierRepository.save(sc.getLastRestockPurchasedFrom()));
-                    supplierSet.add(supplierMap.get(supplierPhoneNumber));
-                }
-            }
-            if (!supplierSet.isEmpty()) sc.setStockPurchasedFrom(supplierSet);
-        }
-
-        sc.setShop(shopInContext);
-
-        return sc;
-    }
-
-    private void readyStockCategoryMap(List<String> sellers){
-        stockCategoryMap = new HashMap<>();
-        stockCategoryMap.putAll(sellers.stream().map(
-            stockCategoryRepository::findAllByCreatedBy).flatMap(List::parallelStream)
-                .collect(Collectors.toMap(StockCategory::getCategoryName, stockCategory -> stockCategory)));
-    }
-
-    private void readySupplierMap(List<String> sellers){
-        supplierMap = new HashMap<>();
-        supplierMap.putAll(sellers.stream().map(
-            supplierRepository::findAllByCreatedBy).flatMap(List::parallelStream)
-                .collect(Collectors.toMap(Supplier::getSupplierPhoneNumber, supplier -> supplier)));
     }
 
     private void readyCustomerMap(List<String> sellers){
